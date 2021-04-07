90c98100b5716497b9a3c945bb2e1063a6a85d19
Demir Dereli <workdemirdereli@gmail.com>
2021-04-08 00:29:06 +0300

    Finnished the Calculator app
    While the design is lacking this is a fully functional basic calculator that is done with vanilla js, html and css. Needs more work to be able to do multiple operations at once

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ let currentNum = null;
 let firstNum = 0;
 let secondNum = 0;
 let operation = null;
+let result = 0;
 
 for(let i = 0; i<buttons.length; i++){
     buttons[i].addEventListener("click", () => {
@@ -33,11 +34,33 @@ for(let i = 0; i < buttons2.length; i++){
 
     })
 }
+resultBtn.addEventListener("click", () => {
+    secondNum = displayNum.innerText;
+    secondNum = parseInt(secondNum);
+    firstNum = parseInt(firstNum);
+    if(operation == "+"){
+        result = firstNum + secondNum;
+        displayNum.innerText = result;
+    } else if(operation == "-"){
+        result = firstNum - secondNum;
+        displayNum.innerText = result;
+    } else if(operation == "*"){
+        result = firstNum * secondNum;
+        displayNum.innerText = result;
+    } else if(operation == "/"){
+        result = firstNum / secondNum;
+        displayNum.innerText = result;
+    } else{
+        displayNum.innerText = "Error!"
+    }
+})
+
 
 resetButton.addEventListener("click", () => {
     currentNum = null;
     firstNum = 0;
     secondNum = 0;
     operation = null;
+    result = 0;
     displayNum.innerText = 0;
 })
